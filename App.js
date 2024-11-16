a798ce0085190efa947b0eaa2e4dafb4cbdb98b7
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-11-16 18:58:01 +0530

    Started Project1 Swiggy

---
+++ App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div>
+        <img src="assets/logo.png" alt="logo" />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<AppLayout />);
