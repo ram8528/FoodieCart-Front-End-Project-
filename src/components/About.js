ed32b18ed383838e3ce42c6efabb55cdbc1f6a92
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-11-20 23:07:40 +0530

    Route regarding About, Error, Contact Only created and called inside APP.js

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <h2>This is Food Delivery About Page</h2>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,10 @@
+const Contact = () => {
+    return (
+        <div>
+            <h1>Contact me on +91-1234567899</h1>
+            <h2>Contact me on sk123hargarh@gmail.com</h2>
+        </div>
+    );
+};
+
+export default Contact;
