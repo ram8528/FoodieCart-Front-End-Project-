57885394325552a828b119cff3bb83f92e117525
ram8528 <106538883+ram8528@users.noreply.github.com>
2024-11-29 19:12:06 +0530

    redux over

--- src/components/ItemList.js
+++ src/components/ItemList.js
@@ -24,9 +24,9 @@ const ItemList = ({ items, dummy }) => {
         <div
           data-testid="foodItems"
           key={item.card.info.id}
-          className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
+          className="p-2 m-2 border-gray-200 border-b-2 text-left flex flex-col sm:flex-row sm:justify-between"
         >
-          <div className="w-9/12">
+          <div className="flex-1 py-2">
             <div className="py-2">
               <span>{item.card.info.name}</span>
               <span>
@@ -38,7 +38,7 @@ const ItemList = ({ items, dummy }) => {
             </div>
             <p className="text-xs">{item.card.info.description}</p>
           </div>
-          <div className="w-3/12 p-4">
+          <div className="w-4/12 p-4">
             <div className="absolute">
               <button
                 className="p-2 mx-16 rounded-lg bg-black text-white shadow-lg" 
--- src/utils/cartSlice.js
+++ src/utils/cartSlice.js
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, current } from "@reduxjs/toolkit"
 
 const cartSlice = createSlice({
     name: 'cart',
@@ -7,14 +7,26 @@ const cartSlice = createSlice({
     },
     reducers: {
         addItem: (state,action) => {
-            // mutating the state here :- we are just modifying state itself
+            // In previous Vanilla Redux ;- Dont mutate state, returning was mandatory
+            // const newState = [...state];
+            // newState.items.push(action.payload);
+            // return newState; 
+
+            // Redux Toolkit uses immer Behind the scenes
+            // mutating the state here :- we are just modifying state itself , now returning is not mandatory redux does it by itself 
             state.items.push(action.payload);
         },
         removeItem: (state) => {
             state.items.pop();
         },
         clearCart : (state) => {
-            state.items.length = 0;
+            // console.log(state); // it will show some proxyobject
+            // console.log(current(state)); // {items: Array(0)}
+            // state = [];
+            // console.log(state);
+            // state = ["Akshay"] -> You are not mutating the state you are just adding a refernce to it
+            state.items.length = 0; // state = [] 
+            // return { item: [] }; // this new object wil be replaced inside originalState = { items: []}
         }
     }
 });
